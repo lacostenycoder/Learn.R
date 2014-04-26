afa00ffae3363e62bbf1de2201b7438a52e47ffe
jemise111 <jesse.sessler@gmail.com>
2014-04-26 15:31:45 -0400

    limit custom user routes to only show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 LearnR::Application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => :registrations }
-  resources :users
+  resources :users, only: [:show]
   root to: "users#index"
 
 end
