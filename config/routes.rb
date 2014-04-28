6755695989ba96b2d31232ffb8804fe4f1e0e33f
davidakimbo <minheeman@gmail.com>
2014-04-28 17:45:34 -0400

    Enabled the showing of categories and lessons in the music index partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ LearnR::Application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => :registrations }
   resources :users, only: [:show]
-  resources :music_lessons, only: [:index, :show]
+  resources :music_lessons, only: [:show]
   resources :code_lessons, only: [:show]
 
   root to: "welcome#index"
-
 
 end
