c56a65375d36e5203d3e081d734a105bcb65ee4b
jemise111 <jesse.sessler@gmail.com>
2014-04-28 10:13:42 -0400

    Working on capybara test for user show page

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 feature 'Page should display list of Lessons started or completed' do
 
+
   background do
-    visit("/user")
-    click_link("Lesson 1")
+    new_user = User.create({email: "somekid@fakemail.com", name: "Matt farts", username: "somekid", password: "butts", password_confirmation: "butts"})
+    sign_in("somekid@fakemail.com", "butts")
   end
 
-  scenario "should take us to the lesson when clicked" do
-    click_button("Code Lesson 1")
-    expect(page).to have_content("Welcome to Lesson 1")
+  scenario "should take us to the code lesson when clicked" do
+    click_link("Code Lesson 1")
+    expect(page).to have_content("Welcome to Code Lesson 1")
   end
 
+  scenario "should take us to the music lesson when clicked" do
+    click_link("Music Lesson 1")
+    expect(page).to have_content("Welcome to Music Lesson 1")
+  end
 
 end
