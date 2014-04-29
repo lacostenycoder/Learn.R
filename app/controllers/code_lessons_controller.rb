19ee990187cf931e9cc819f56968ab30aa918c50
jemise111 <jesse.sessler@gmail.com>
2014-04-29 10:16:04 -0400

    partial commit, add some code lesson logic

--- app/controllers/code_lessons_controller.rb
+++ app/controllers/code_lessons_controller.rb
@@ -2,6 +2,10 @@ class CodeLessonsController < ApplicationController
 
   def show
     @lesson = CodeLesson.find(params[:id])
+    respond_to do |format|
+      format.html { }
+      format.json { render json: @lesson.to_json }
+    end
   end
 
 end
