e6dac2c6fe661cab8c3114d26a47fbfb60944ccb
lacostenycoder <djlancejordan@gmail.com>
2014-04-30 11:02:04 -0400

    music lesson updating scores working

--- app/controllers/music_lessons_controller.rb
+++ app/controllers/music_lessons_controller.rb
@@ -8,4 +8,14 @@ class MusicLessonsController < ApplicationController
     end
   end
 
+    def update_win
+    lesson = MusicLesson.find(params[:id])
+    unless current_user.music_lessons.include?(lesson)
+      current_user.music_lessons << lesson
+      current_user.score += lesson.points
+      current_user.save
+    end
+    @next_lesson = MusicLesson.find_by(global_level: lesson.global_level + 1)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 LearnR::Application.routes.draw do
 
   post '/code_lessons/:id/update-win' => 'code_lessons#update_win'
+  post '/music_lessons/:id/update-win' => 'music_lessons#update_win'
   devise_for :users, :controllers => { :registrations => :registrations }
   resources :users, only: [:show]
   resources :music_lessons, only: [:show]
