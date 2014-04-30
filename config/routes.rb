8bf2012dda15943c097d2c42fdf997d121ea184b
davidakimbo <minheeman@gmail.com>
2014-04-30 18:29:34 -0400

    add admin functionality for music lessons

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ LearnR::Application.routes.draw do
   post '/music_lessons/:id/update-win' => 'music_lessons#update_win'
   devise_for :users, :controllers => { :registrations => :registrations }
   resources :users, only: [:index, :show]
-  resources :music_lessons, only: [:show]
-  resources :code_lessons, only: [:show]
+  resources :music_lessons
+  resources :code_lessons
 
   root to: "welcome#index"
-
 end
